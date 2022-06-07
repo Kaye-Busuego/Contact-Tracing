cdf0711ce9e649bf7ee0cda588fa3f18f7c190ed
Kaye Busuego <kayesbusuego@iskolarngbayan.pup.edu.ph>
2022-06-07 15:56:23 +0800

    Textboxes renamed

--- Contact-Tracing/Form1.Designer.cs
+++ Contact-Tracing/Form1.Designer.cs
@@ -31,19 +31,21 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox1Name = new System.Windows.Forms.TextBox();
+            this.textBox4Sex = new System.Windows.Forms.TextBox();
+            this.textBox5Contact = new System.Windows.Forms.TextBox();
+            this.textBox2Age = new System.Windows.Forms.TextBox();
+            this.textBox3Address = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox6Temp = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox7DOV = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.textBox8RFV = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -75,45 +77,45 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Name";
             // 
-            // textBox1
+            // textBox1Name
             // 
-            this.textBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(126, 59);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(270, 27);
-            this.textBox1.TabIndex = 1;
+            this.textBox1Name.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1Name.Location = new System.Drawing.Point(126, 59);
+            this.textBox1Name.Name = "textBox1Name";
+            this.textBox1Name.Size = new System.Drawing.Size(270, 27);
+            this.textBox1Name.TabIndex = 1;
             // 
-            // textBox2
+            // textBox4Sex
             // 
-            this.textBox2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox2.Location = new System.Drawing.Point(126, 158);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(270, 27);
-            this.textBox2.TabIndex = 4;
+            this.textBox4Sex.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox4Sex.Location = new System.Drawing.Point(126, 158);
+            this.textBox4Sex.Name = "textBox4Sex";
+            this.textBox4Sex.Size = new System.Drawing.Size(270, 27);
+            this.textBox4Sex.TabIndex = 4;
             // 
-            // textBox3
+            // textBox5Contact
             // 
-            this.textBox3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox3.Location = new System.Drawing.Point(126, 191);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(270, 27);
-            this.textBox3.TabIndex = 5;
+            this.textBox5Contact.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox5Contact.Location = new System.Drawing.Point(126, 191);
+            this.textBox5Contact.Name = "textBox5Contact";
+            this.textBox5Contact.Size = new System.Drawing.Size(270, 27);
+            this.textBox5Contact.TabIndex = 5;
             // 
-            // textBox4
+            // textBox2Age
             // 
-            this.textBox4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox4.Location = new System.Drawing.Point(126, 92);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(270, 27);
-            this.textBox4.TabIndex = 6;
+            this.textBox2Age.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2Age.Location = new System.Drawing.Point(126, 92);
+            this.textBox2Age.Name = "textBox2Age";
+            this.textBox2Age.Size = new System.Drawing.Size(270, 27);
+            this.textBox2Age.TabIndex = 6;
             // 
-            // textBox5
+            // textBox3Address
             // 
-            this.textBox5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox5.Location = new System.Drawing.Point(126, 125);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(270, 27);
-            this.textBox5.TabIndex = 7;
+            this.textBox3Address.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3Address.Location = new System.Drawing.Point(126, 125);
+            this.textBox3Address.Name = "textBox3Address";
+            this.textBox3Address.Size = new System.Drawing.Size(270, 27);
+            this.textBox3Address.TabIndex = 7;
             // 
             // label4
             // 
@@ -155,13 +157,13 @@
             this.label6.TabIndex = 12;
             this.label6.Text = "Contact #";
             // 
-            // textBox6
+            // textBox6Temp
             // 
-            this.textBox6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox6.Location = new System.Drawing.Point(126, 224);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(270, 27);
-            this.textBox6.TabIndex = 13;
+            this.textBox6Temp.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox6Temp.Location = new System.Drawing.Point(126, 224);
+            this.textBox6Temp.Name = "textBox6Temp";
+            this.textBox6Temp.Size = new System.Drawing.Size(270, 27);
+            this.textBox6Temp.TabIndex = 13;
             // 
             // label7
             // 
@@ -173,13 +175,13 @@
             this.label7.TabIndex = 14;
             this.label7.Text = "Temperature";
             // 
-            // textBox7
+            // textBox7DOV
             // 
-            this.textBox7.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox7.Location = new System.Drawing.Point(126, 257);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(270, 27);
-            this.textBox7.TabIndex = 15;
+            this.textBox7DOV.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox7DOV.Location = new System.Drawing.Point(126, 257);
+            this.textBox7DOV.Name = "textBox7DOV";
+            this.textBox7DOV.Size = new System.Drawing.Size(270, 27);
+            this.textBox7DOV.TabIndex = 15;
             // 
             // label8
             // 
@@ -191,26 +193,47 @@
             this.label8.TabIndex = 16;
             this.label8.Text = "Date of Visit";
             // 
+            // textBox8RFV
+            // 
+            this.textBox8RFV.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox8RFV.Location = new System.Drawing.Point(126, 299);
+            this.textBox8RFV.Name = "textBox8RFV";
+            this.textBox8RFV.Size = new System.Drawing.Size(270, 27);
+            this.textBox8RFV.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(14, 284);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(106, 46);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Reason for \r\nVisitation";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1014, 377);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8RFV);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox7DOV);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox6Temp);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3Address);
+            this.Controls.Add(this.textBox2Age);
+            this.Controls.Add(this.textBox5Contact);
+            this.Controls.Add(this.textBox4Sex);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox1Name);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.ShowIcon = false;
@@ -225,18 +248,20 @@
         private Button button1;
         private Label label1;
         private Label label2;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
+        private TextBox textBox1Name;
+        private TextBox textBox4Sex;
+        private TextBox textBox5Contact;
+        private TextBox textBox2Age;
+        private TextBox textBox3Address;
         private Label label4;
         private Label label3;
         private Label label5;
         private Label label6;
-        private TextBox textBox6;
+        private TextBox textBox6Temp;
         private Label label7;
-        private TextBox textBox7;
+        private TextBox textBox7DOV;
         private Label label8;
+        private TextBox textBox8RFV;
+        private Label label9;
     }
 }
